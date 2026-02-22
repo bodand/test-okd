b74726ccf94f8e282924b82cb0cf1f1d789d5bd1
bodand <bodand@proton.me>
2026-02-22 21:57:46 +0100

    feat: fancy

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    # Itt közvetlenül adjuk vissza a HTML-t
     return """
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Üdvözlet</title>
+        <title>Üdvözlet 2</title>
     </head>
     <body>
         <h1>Hello :)</h1>
